809b3f3125f465167856ceaa36da42764cf76679
muli c <muli3203@gmail.com>
2019-10-22 15:16:05 +0300

    added new changes to config .py

--- config.py
+++ config.py
@@ -1,22 +1,41 @@
+# class Config:
+#     '''
+#     general configuration parent class
+#     '''
+#     pass
+
+# class ProdConfig(Config):
+#     '''
+#     production configuation child class
+#     ''''
+    
+# class DevConfig(Config):
+#     '''
+#     development configuration  childclass
+#     '''
+#     pass
+
+#     DEBUG = True
+
+
+
+ import os
+
 class Config:
-    '''
-    general configuration parent class
-    '''
-    pass
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=f9b3923ad99d4449b0e79a0241931c05'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 class ProdConfig(Config):
-    '''
-    production configuation child class
-    '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=f9b3923ad99d4449b0e79a0241931c05'
-    
-class DevConfig(Config):
-    '''
-    development configuration  childclass
-    '''
     pass
 
+
+class DevConfig(Config):
     DEBUG = True
 
-
-
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
